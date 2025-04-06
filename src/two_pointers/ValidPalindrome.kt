238b7734c817f4694200dda86bf56743f41b58d1
oleksandrhoncharuk <oleksandr.honcharuk1@gmail.com>
2025-04-06 20:40:33 +0300

    Valid Palindrome

---
+++ src/two_pointers/ValidPalindrome.kt
@@ -0,0 +1,53 @@
+package two_pointers
+
+/**
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+ *
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.
+ * Example 2:
+ *
+ * Input: s = "race a car"
+ * Output: false
+ * Explanation: "raceacar" is not a palindrome.
+ * Example 3:
+ *
+ * Input: s = " "
+ * Output: true
+ * Explanation: s is an empty string "" after removing non-alphanumeric characters.
+ * Since an empty string reads the same forward and backward, it is a palindrome.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 2 * 105
+ * s consists only of printable ASCII characters.
+ */
+
+fun isPalindrome(s: String): Boolean {
+    var result = true
+
+    val str = s.lowercase().filter { it in 'a'..'z' || it in '0'..'9'}
+
+    for ((index, char) in str.withIndex()) {
+        if (char == str.elementAt(str.lastIndex - index)) {
+            result = true
+        } else {
+            result = false
+            break
+        }
+        if (index >= str.lastIndex - index) break
+    }
+    return result
+}
+
+fun main() {
+    println(isPalindrome("A man, a plan, a canal: Panama"))
+}
