5ff9848b304d36b8246821ae00d0cd7ecba353d2
oleksandrhoncharuk <oleksandr.honcharuk1@gmail.com>
2025-04-20 22:07:22 +0300

    20, Valid parentheses. Not optimised

---
+++ src/stack/ValidParentheses.kt
@@ -0,0 +1,68 @@
+package stack
+
+import java.util.Stack
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "()"
+ *
+ * Output: true
+ *
+ * Example 2:
+ *
+ * Input: s = "()[]{}"
+ *
+ * Output: true
+ *
+ * Example 3:
+ *
+ * Input: s = "(]"
+ *
+ * Output: false
+ *
+ * Example 4:
+ *
+ * Input: s = "([])"
+ *
+ * Output: true
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 104
+ * s consists of parentheses only '()[]{}'.
+ */
+
+fun isValid(s: String): Boolean {
+    val openToClose = mapOf(')' to '(', ']' to '[', '}' to '{')
+    val stack = Stack<Char>()
+
+    s.forEach { char ->
+        if (openToClose.contains(char)) {
+            if (stack.isNotEmpty() && stack.last() == openToClose[char]) {
+                stack.pop()
+            } else {
+                return false
+            }
+        } else {
+            stack.add(char)
+        }
+    }
+
+    return stack.isEmpty()
+}
+
+fun main() {
+    println(isValid("(]"))
+}
