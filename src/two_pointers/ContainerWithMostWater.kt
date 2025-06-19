bda2579db783449573de9e671d8dba663ea9a6d7
oleksandrhoncharuk <oleksandr.honcharuk1@gmail.com>
2025-06-19 10:20:39 +0300

    Text Justification

---
+++ src/two_pointers/ContainerWithMostWater.kt
@@ -0,0 +1,57 @@
+package two_pointers
+
+/**
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ *
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ *
+ * Return the maximum amount of water a container can store.
+ *
+ * Notice that you may not slant the container.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+ * Example 2:
+ *
+ * Input: height = [1,1]
+ * Output: 1
+ *
+ *
+ * Constraints:
+ *
+ * n == height.length
+ * 2 <= n <= 105
+ * 0 <= height[i] <= 104
+ */
+
+fun maxArea(height: IntArray): Int {
+    var left = 0
+    var right = height.lastIndex
+    var maxArea = 0
+
+    while (left < right) {
+        val width = right - left
+        val h = minOf(height[left], height[right])
+        val area = width * h
+        maxArea = maxOf(maxArea, area)
+
+        // Move the pointer at the shorter line
+        if (height[left] < height[right]) {
+            left++
+        } else {
+            right--
+        }
+    }
+
+    return maxArea
+}
+
+fun main() {
+    println(maxArea(intArrayOf(1,8,6,2,5,4,8,3,7)))
+}
